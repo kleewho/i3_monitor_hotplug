1bb44f2a4d381f61ccd76d7f18fb477350d526a9
Lukasz Klich <lukasz.klich@upc.pl>
2014-03-29 11:57:54 +0100

    New function moves workspaces from disabled outputs to first active

--- hotplug.rb
+++ hotplug.rb
@@ -2,15 +2,28 @@
 
 require 'json'
 
+def move_workspaces_from_disabled_outputs()
+  active_outputs = outputs
+
+  workspaces.reject{ |ws|
+    ws['output'] == active_outputs.first['name']
+  }.each{ |ws|
+    i3_set_current ws['name']
+    puts 'Current workspace set to ' + ws['name']
+    move_workspace_to_output active_outputs.first['name']
+  }
+end
+
 def workspaces()
-  workspaces = JSON.parse(i3_workspaces)
-  puts workspaces
+  JSON.parse(i3_workspaces)
 end
 
 def outputs()
-  outputs = JSON.parse(i3_outputs).reject{|output| output['active'] == false}
-  puts outputs
-  puts "Outputs"
+  JSON.parse(i3_outputs).reject{|output| output['active'] == false}
+end
+
+def i3_set_current(workspace)
+  i3_command "workspace #{workspace}"
 end
 
 def i3_msg(command)
@@ -18,6 +31,7 @@ def i3_msg(command)
 end
 
 def i3_command(command)
+#  JSON.parse(i3_msg "command #{command}")[0]["success"]
   i3_msg "command #{command}"
 end
 
@@ -27,6 +41,10 @@ end
 
 def i3_workspaces()
   i3_msg 'get_workspaces'
+end
+
+def move_workspace_to_output(output)
+  i3_command "move workspace to output #{output}"
 end
 
 def init()
